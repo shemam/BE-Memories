e62ad6fe1af4b068e6919903bc8a80b9714e8b3c
shema <shemamugisha>
2022-03-22 12:03:53 +0200

    add task validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,11 +7,14 @@ import {
   Patch,
   Post,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -28,12 +31,13 @@ export class TasksController {
     }
   }
 
-  @Get(':id')
+  @Get('/:id')
   getTaskById(@Param('id') id: string): Task {
     return this.tasksService.getTaskByID(id);
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
   }
@@ -46,7 +50,7 @@ export class TasksController {
   @Patch(':id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
   ) {
     return this.tasksService.updateTaskStatus(id, status);
   }
